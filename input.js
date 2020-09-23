c295084685e4fdfa73a3da543898eae5d317a8e7
Alexander Scaini <alexanderscaini@gmail.com>
2020-09-23 18:36:35 +0000

    added movement keys and a hey message display

---
+++ input.js
@@ -0,0 +1,32 @@
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', (data) => {
+    conn.write(handleUserInput(data))
+  })
+  return stdin;
+}
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  } else if (key === 'a') {
+    return ("Move: left")
+  } else if (key === 'w') {
+    return ("Move: up")
+  } else if (key === 's') {
+    return ("Move: down")
+  } else if (key === 'd') {
+    return ("Move: right")
+  } else if (key === 'g') {
+    return ('Name: GG')
+  }
+  
+}
+
+module.exports = {setupInput}
